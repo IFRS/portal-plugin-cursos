4ee5fd017e4b6a4c3917d7d573505d4252b8a5f2
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2019-06-17 17:17:33 -0300

    Remoção do carregamento direto do CMB2 devido a bugs

--- ifrs-portal-plugin-cursos.php
+++ ifrs-portal-plugin-cursos.php
@@ -3,7 +3,7 @@ defined('ABSPATH') or die('No script kiddies please!');
 /*
 Plugin Name: IFRS Portal Cursos
 Plugin URI:  https://github.com/IFRS/portal-plugin-cursos
-Description: Plugin para gerenciar Cursos.
+Description: Plugin para gerenciamento de Cursos.
 Version:     0.0.1
 Author:      Ricardo Moro
 Author URI:  https://github.com/ricardomoro
@@ -12,12 +12,6 @@ License URI: https://www.gnu.org/licenses/gpl-3.0.txt
 Text Domain: ifrs-portal-plugin-cursos
 Domain Path: /lang
 */
-
-if ( file_exists( dirname( __FILE__ ) . '/vendor/cmb2/init.php' ) ) {
-	require_once dirname( __FILE__ ) . '/vendor/cmb2/init.php';
-} elseif ( file_exists( dirname( __FILE__ ) . '/vendor/CMB2/init.php' ) ) {
-	require_once dirname( __FILE__ ) . '/vendor/CMB2/init.php';
-}
 
 require_once('curso.php');
 require_once('curso-modalidade.php');
