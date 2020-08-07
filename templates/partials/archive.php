44c0278b05972e55e3c75bd229dace188a2fe4d6
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2020-08-07 17:02:54 -0300

    Melhor organização do código

--- templates/partials/archive.php
+++ templates/partials/archive.php
@@ -28,34 +28,7 @@
         <?php if (have_posts()) : ?>
             <div class="cursos__content">
             <?php while(have_posts()) : the_post(); ?>
-                <div class="card curso-item">
-                    <div class="card-header">
-                        <?php foreach (get_the_terms(get_the_ID(), 'curso_unidade') as $unidade) : ?>
-                            <span class="curso-item__unidade"><?php echo $unidade->name; ?></span>
-                        <?php endforeach; ?>
-                    </div>
-                    <div class="card-body">
-                        <h2 class="card-title curso-item__title"><a href="<?php the_permalink(); ?>" class="curso-item__link"><?php the_title(); ?></a></h2>
-                        <p class="card-text">
-                            <?php $niveis = wp_get_post_terms(get_the_ID(), 'curso_nivel', array('orderby' => 'term_id')); ?>
-                            <?php foreach ($niveis as $nivel) : ?>
-                                <span class="curso-item__nivel">
-                                    <?php echo $nivel->name; ?>
-                                    <?php echo ($nivel !== end($niveis)) ? '<strong> / </strong>' : ''; ?>
-                                </span>
-                            <?php endforeach; ?>
-                            <?php foreach (get_the_terms(get_the_ID(), 'curso_modalidade') as $modalidade) : ?>
-                                <span class="curso-item__modalidade"><?php echo $modalidade->name; ?></span>
-                            <?php endforeach; ?>
-                        </p>
-                    </div>
-                    <div class="card-footer">
-                        <?php $turnos = wp_get_post_terms(get_the_ID(), 'curso_turno', array('orderby' => 'term_order')); ?>
-                        <?php foreach ($turnos as $turno) : ?>
-                            <span class="curso-item__turnos"><?php echo $turno->name; echo ($turno !== end($turnos)) ? ', ' : ''; ?></span>
-                        <?php endforeach; ?>
-                    </div>
-                </div>
+                <?php load_template(plugin_dir_path(__FILE__) . 'item.php', false); ?>
             <?php endwhile; ?>
             </div>
         <?php else : ?>
