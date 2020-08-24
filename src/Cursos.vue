8e49c6dfeda092a750236537be7075c423e773ef
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2020-08-24 17:17:01 -0300

    Criação inicial da paginação de Cursos

--- src/Cursos.vue
+++ src/Cursos.vue
@@ -32,6 +32,21 @@
         </div>
       </div>
     </div>
+    <div class="col-12" v-if="pages && pages > 1">
+      <nav aria-label="Paginação de Cursos">
+          <ul class="pagination justify-content-center">
+            <li class="page-item" :class="(page == 1) ? 'disabled' : ''">
+              <button class="page-link" :disabled="(page == 1)" @click="page = page - 1">Anterior</button>
+            </li>
+            <li class="page-item" v-for="n in pages" :key="n" :class="(page == n) ? 'disabled' : ''">
+              <button class="page-link" :disabled="(page == n)" @click="page = n">{{n}}</button>
+            </li>
+            <li class="page-item" :class="(page == pages) ? 'disabled' : ''">
+              <button class="page-link" :disabled="(page == pages)" @click="page = page + 1">Pr&oacute;xima</button>
+            </li>
+          </ul>
+        </nav>
+    </div>
   </div>
   <div v-else class="text-center">
     <Loading/>
@@ -49,13 +64,33 @@ export default {
   data() {
     return {
       cursos: null,
+      pages: null,
+      page: 1,
     }
   },
   mounted() {
-    this.$axios.get('/cursos')
-    .then(response => {
-      this.cursos = response.data;
-    });
+    this.getCursos();
+  },
+  methods: {
+    getCursos() {
+      this.$axios.get('/cursos', {
+        params: {
+          order: 'asc',
+          orderby: 'title',
+          page: this.page,
+          per_page: 1,
+        }
+      })
+      .then(response => {
+        this.cursos = response.data;
+        this.pages = parseInt(response.headers['x-wp-totalpages']);
+      });
+    },
+  },
+  watch: {
+    page: function(newPage) {
+      this.getCursos();
+    },
   },
 }
 </script>
