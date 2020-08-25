2cd58e55da2db3cb8c227df9d94b8ff590b10012
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2020-08-25 12:27:42 -0300

    Exibição de loading para imagem destaque

--- src/Curso.vue
+++ src/Curso.vue
@@ -5,6 +5,7 @@
         <h2 class="curso__title">{{curso.title.rendered}}</h2>
         <div class="curso__content">
           <img class="curso__thumb" v-if="media" :src="media.source_url" :alt="media.alt_text"/>
+          <Loading width="100" class="curso__thumb" v-if="!media && media_loading"/>
           <template v-html="curso.content.rendered"/>
           <h3>Pr&eacute;-requisitos</h3>
           <p v-for="(requisito, r) in curso.meta_box._curso_nivel_taxonomy" :key="r">
@@ -51,7 +52,6 @@
               <div class="curso-aside__content">
                 <div class="curso-info">
                   <svg xmlns="http://www.w3.org/2000/svg" class="curso-info__icon" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                    <path stroke="none" d="M0 0h24v24H0z"/>
                     <path d="M16 16v2a2 2 0 0 1 -2 2h-8a2 2 0 0 1 -2 -2v-8a2 2 0 0 1 2 -2h2v-2a2 2 0 0 1 2 -2h8a2 2 0 0 1 2 2v8a2 2 0 0 1 -2 2h-2" />
                   </svg>
                   <h4 class="curso-info__title">Modalidade</h4>
@@ -59,7 +59,6 @@
                 </div>
                 <div class="curso-info">
                   <svg xmlns="http://www.w3.org/2000/svg" class="curso-info__icon" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                    <path stroke="none" d="M0 0h24v24H0z"/>
                     <line x1="3" y1="21" x2="21" y2="21" />
                     <line x1="9" y1="8" x2="10" y2="8" />
                     <line x1="9" y1="12" x2="10" y2="12" />
@@ -74,7 +73,6 @@
                 </div>
                 <div class="curso-info">
                   <svg xmlns="http://www.w3.org/2000/svg" class="curso-info__icon" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                    <path stroke="none" d="M0 0h24v24H0z"/>
                     <rect x="3" y="15" width="6" height="6" rx="2" />
                     <rect x="15" y="15" width="6" height="6" rx="2" />
                     <rect x="9" y="3" width="6" height="6" rx="2" />
@@ -88,7 +86,6 @@
                 </div>
                 <div class="curso-info">
                   <svg xmlns="http://www.w3.org/2000/svg" class="curso-info__icon" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                    <path stroke="none" d="M0 0h24v24H0z"/>
                     <circle cx="12" cy="12" r="4" />
                     <path d="M3 12h1M12 3v1M20 12h1M12 20v1M5.6 5.6l.7 .7M18.4 5.6l-.7 .7M17.7 17.7l.7 .7M6.3 17.7l-.7 .7" />
                   </svg>
@@ -99,7 +96,6 @@
                 </div>
                 <div class="curso-info">
                   <svg xmlns="http://www.w3.org/2000/svg" class="curso-info__icon" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                    <path stroke="none" d="M0 0h24v24H0z"/>
                     <rect x="4" y="5" width="16" height="16" rx="2" />
                     <line x1="16" y1="3" x2="16" y2="7" />
                     <line x1="8" y1="3" x2="8" y2="7" />
@@ -111,7 +107,6 @@
                 </div>
                 <div class="curso-info" v-if="curso.meta_box._curso_nota">
                   <svg xmlns="http://www.w3.org/2000/svg" class="curso-info__icon" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                    <path stroke="none" d="M0 0h24v24H0z"/>
                     <path d="M12 17.75l-6.172 3.245 1.179-6.873-4.993-4.867 6.9-1.002L12 2l3.086 6.253 6.9 1.002-4.993 4.867 1.179 6.873z" />
                   </svg>
                   <h4 class="curso-info__title">Avalia&ccedil;&atilde;o do Curso</h4>
@@ -156,6 +151,7 @@ export default {
     return {
       curso: null,
       media: null,
+      media_loading: false,
     }
   },
   mounted() {
@@ -171,9 +167,16 @@ export default {
   watch: {
     curso: function(curso) {
       if (curso.featured_media) {
+        this.media_loading = true;
         this.$axios.get('/media/' + curso.featured_media)
         .then(response => {
           this.media = response.data;
+        })
+        .catch(error => {
+          console.error(error);
+        })
+        .then(() => {
+          this.media_loading = false;
         });
       }
     },
