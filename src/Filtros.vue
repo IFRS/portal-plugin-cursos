6c5de96da6b995f71a7c8ba6301f9b3def5635dc
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2020-08-27 15:46:46 -0300

    Adição de busca textual, desabilitação de ações e ajustes gerais

--- src/Filtros.vue
+++ src/Filtros.vue
@@ -1,10 +1,10 @@
 <template>
   <aside class="filter">
     <h3 class="filter__title">Filtros</h3>
-    <form class="filter__form" v-on:submit.prevent="filtrar()">
+    <form class="filter__form" @submit.prevent="filtrar()">
       <div class="input-group">
-        <label class="sr-only" for="">Termo para busca</label>
-        <input class="form-control form-control-sm" type="text" name="s" value="" id="" placeholder="Buscar cursos..."/>
+        <label class="sr-only" for="ifrs-cursos-filtros-search">Termo para busca</label>
+        <input class="form-control form-control-sm" type="text" v-model="filtros.search" id="ifrs-cursos-filtros-search" placeholder="Buscar cursos..."/>
       </div>
       <fieldset>
         <legend>Modalidade</legend>
@@ -21,29 +21,31 @@
         </div>
       </fieldset>
       <fieldset>
-          <legend>N&iacute;vel</legend>
-          <template v-for="(nivel, n) in niveis">
-            <div class="form-check" :key="n">
-              <input class="form-check-input" type="checkbox" v-model="filtros.curso_nivel" :value="nivel.id" :id="'nivel-' + nivel.id">
-              <label class="form-check-label" :for="'nivel-' + nivel.id">{{nivel.name}}</label>
-            </div>
-            <div class="form-check ml-3" v-for="filho in nivel.children" :key="filho.id">
-              <input class="form-check-input" type="checkbox" v-model="filtros.curso_nivel" :value="filho.id" :id="'nivel-' + filho.id">
-              <label class="form-check-label" :for="'nivel-' + filho.id">{{filho.name}}</label>
-            </div>
-          </template>
+        <legend>N&iacute;vel</legend>
+        <template v-for="(nivel, n) in niveis">
+          <div class="form-check" :key="n">
+            <input class="form-check-input" type="checkbox" v-model="filtros.curso_nivel" :value="nivel.id" :id="'nivel-' + nivel.id">
+            <label class="form-check-label" :for="'nivel-' + nivel.id">{{nivel.name}}</label>
+          </div>
+          <div class="form-check ml-3" v-for="filho in nivel.children" :key="filho.id">
+            <input class="form-check-input" type="checkbox" v-model="filtros.curso_nivel" :value="filho.id" :id="'nivel-' + filho.id">
+            <label class="form-check-label" :for="'nivel-' + filho.id">{{filho.name}}</label>
+          </div>
+        </template>
       </fieldset>
       <fieldset>
-          <legend>Turno</legend>
-          <div class="form-check form-check-inline" v-for="(turno, t) in turnos" :key="t">
-            <input class="form-check-input" type="checkbox" v-model="filtros.curso_turno" :value="turno.id" :id="'turno-' + turno.id" >
-            <label class="form-check-label" :for="'turno-' + turno.id">{{turno.name}}</label>
-          </div>
+        <legend>Turno</legend>
+        <div class="form-check form-check-inline" v-for="(turno, t) in turnos" :key="t">
+          <input class="form-check-input" type="checkbox" v-model="filtros.curso_turno" :value="turno.id" :id="'turno-' + turno.id" >
+          <label class="form-check-label" :for="'turno-' + turno.id">{{turno.name}}</label>
+        </div>
       </fieldset>
-      <div class="btn-group" role="group" aria-label="Ações do Filtro">
-          <input type="submit" value="Filtrar" class="btn btn-primary"/>
-          <button class="btn btn-outline-secondary" @click="limpar">Limpar Filtros</button>
-      </div>
+      <fieldset class="text-center">
+        <div class="btn-group" role="group" aria-label="Ações do Filtro">
+          <button type="submit" class="btn btn-primary" :disabled="disabledActions">Filtrar<span class="sr-only">&nbsp;Cursos</span></button>
+          <button type="reset" class="btn btn-outline-secondary" @click="limpar()">Limpar<span class="sr-only">&nbsp;filtros</span></button>
+        </div>
+      </fieldset>
     </form>
   </aside>
 </template>
@@ -58,12 +60,28 @@ export default {
       niveis: null,
       turnos: null,
       filtros: {
+        search: '',
         curso_modalidade: [],
         curso_unidade: [],
         curso_nivel: [],
         curso_turno: [],
       },
     }
+  },
+  computed: {
+    disabledActions: function() {
+      return (
+        this.filtros.search === ''
+        &&
+        this.filtros.curso_modalidade.length === 0
+        &&
+        this.filtros.curso_unidade.length === 0
+        &&
+        this.filtros.curso_nivel.length === 0
+        &&
+        this.filtros.curso_turno.length === 0
+      );
+    },
   },
   mounted() {
     this.getModalidades();
@@ -77,6 +95,7 @@ export default {
     },
     limpar() {
       this.filtros = {
+        search: '',
         curso_modalidade: [],
         curso_unidade: [],
         curso_nivel: [],
