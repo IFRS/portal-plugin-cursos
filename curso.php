78025edc9a6e81131ad8b758fd78b2f4d43a405a
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2021-03-17 12:15:40 -0300

    Correção no carregamento do template para página de cursos

--- curso.php
+++ curso.php
@@ -335,7 +335,7 @@ add_filter( 'single_template', function($template) {
 } );
 
 add_filter( 'page_template', function($template) {
-    if (empty(locate_template('page-cursos.php', false))) {
+    if (is_page('cursos') && empty(locate_template('page-cursos.php', false))) {
         return plugin_dir_path(__FILE__) . 'templates/page-cursos.php';
     }
 
